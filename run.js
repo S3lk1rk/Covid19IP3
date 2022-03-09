d1c6ed835d21562c6d30f19310b28d8b14485312
“S3lk1rk” <matthew.selkirk51@gmail.com>
2022-03-09 17:39:37 +0000

    first commit

---
+++ run.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const path = require('path');
+const public = path.join(__dirname,'public');
+
+app.use(express.static(public));
+
+
+ 
+app.get('/map', function(req, res)
+{ res.sendFile(path.join(public, 'map.html'));
+})
+
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000. Ctrl^c to quit.'); })
