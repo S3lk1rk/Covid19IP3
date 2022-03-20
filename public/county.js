c9cdefddf78ae96f149be895364b45d70b3723a9
NKIER200 <nkier200@caledonian.ac.uk>
2022-03-20 23:28:37 +0000

    fat hog

--- public/county.js
+++ public/county.js
@@ -4,4 +4,11 @@ function storeVar(area) {
     console.log(county);
   } 
 
-  
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
