82f73d445e2e8bb3220f2ef01cdc1c88ae47739f
KaranPatel186 <69085636+KaranPatel186@users.noreply.github.com>
2022-04-18 18:01:18 +0100

    added styling

--- run.js
+++ run.js
@@ -7,14 +7,20 @@ const public = path.join(__dirname,'public');
 app.use(express.static(public));
 
 app.get('/', function(req, res)
-{ res.sendFile(path.join(public, 'home.html'));
+{ res.sendFile(path.join(public, 'about.html'));
 })
  
 app.get('/map', function(req, res)
 { res.sendFile(path.join(public, 'map.html'));
 })
 
+app.get('/about', function(req, res)
+{ res.sendFile(path.join(public, 'about.html'));
+})
 
+app.get('/contact', function(req, res)
+{ res.sendFile(path.join(public, 'contact.html'));
+})
 
 app.listen(3000, () => {
     console.log('Server started on port 3000. Ctrl^c to quit.'); })
