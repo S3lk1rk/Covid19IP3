981f3919a191f9abbe7dc6ee035e236f5aa4e212
KaranPatel186 <69085636+KaranPatel186@users.noreply.github.com>
2022-04-24 00:10:17 +0100

    Final Commit

--- public/county.js
+++ public/county.js
@@ -1,18 +1,27 @@
+function darkMode() {                                                           //dark mode function for map page
+    var element = document.body;                                                //sets varaible element to the body
+    element.classList.toggle("dark-mode1");                                     //toggles dark mode class
+};
+
+function darkModeAbout() {                                                      //dark mode function
+    var element = document.body;                                                //sets varaible element to the body
+    element.classList.toggle("dark-mode2");                                     //toggles dark mode class
+};
 
 
-function storeVar(area) {
-  var county = area.getAttribute('value');
-  document.getElementById("searchbar").value = county;
-  console.log(county);
+function storeVar(area) {                                                       //function to store varaibles
+  var county = area.getAttribute('value');                                      //sets varaible county to the value of the area
+  document.getElementById("searchbar").value = county;                          //sets the value of searchbar to the value of county
+  console.log(county);                                                          //prints county to console
 
 
 
-var x = document.getElementById("searchbar").value;
+var x = document.getElementById("searchbar").value;                             //sets varaible x to the value of searchbar
 
   /**
    * Extracts paginated data by requesting all of the pages
    * and combining the results.
-   * @param filters { Array<string> }
+   * @param filters { Array<string> }                                           
    *          API filters. See the API documentations for additional
    *          information.
    * @param structure { Object<string, any> }
@@ -23,41 +32,41 @@ var x = document.getElementById("searchbar").value;
    *          the given ``filters`` and ``structure``.
    */
   
-  const getPaginatedData = async ( filters, structure ) => {
+  const getPaginatedData = async ( filters, structure ) => {                    //declares function getPaginatedData
       const
-          endpoint = 'https://api.coronavirus.data.gov.uk/v1/data',
-          apiParams = {
+          endpoint = 'https://api.coronavirus.data.gov.uk/v1/data',             //declares endpoint
+          apiParams = {                                                         //declares apiParams
               filters: filters.join(";"),
               structure: JSON.stringify(structure)
           },
           result = [];
-      let
-          nextPage = null,
+      let                                                                       //declares varaibles
+          nextPage = null,      
           currentPage = 1;
       do {
-          const { data, status, statusText } = await axios.get(endpoint, {
-              params: {
+          const { data, status, statusText } = await axios.get(endpoint, {      //sets varaibles to the data, status and statusText of axios.get
+              params: {                                                         //sets params to the apiParams
                   ...apiParams,
                   page: currentPage
               },
-              timeout: 10000
+              timeout: 10000                                                    //sets timeout to 10 seconds
           });
-          if ( status >= 400 )
-              throw Error(statusText);
-          if ( "pagination" in data )
-              nextPage = data.pagination.next || null;
-          result.push(...data.data);  
-          currentPage ++;
-      } while ( nextPage );
-      return result;
+          if ( status >= 400 )                                                  //if status is greater than or equal to 400
+              throw Error(statusText);                                          //throw error
+          if ( "pagination" in data )                                           //if pagination in data
+              nextPage = data.pagination.next || null;                          //set nextPage to the value of data.pagination.next or null
+          result.push(...data.data);                                            //push data.data to result
+          currentPage ++;                                                       //add 1 to currentPage
+      } while ( nextPage );                                                    
+      return result;                                                            //return result
   };  // getData
 
-  const main = async () => {
+  const main = async () => {                                                    //declares function main
       const
-          filters = [
-              `areaType=utla;areaName=` + x +'&',
+          filters = [                                                           //declares filters         
+              `areaType=utla;areaName=` + x +'&',                               
           ],
-          structure = {
+          structure = {                                                         //declares structure for api call
               Date: "date",
               Name: "areaName",  
               Vaccinated: "cumPeopleVaccinatedFirstDoseByPublishDate",
@@ -66,12 +75,12 @@ var x = document.getElementById("searchbar").value;
               Cumulative: "cumCasesBySpecimenDate",
               Deaths: "cumDeaths28DaysByDeathDate"
           };
-      const results = await getPaginatedData(filters, structure);
-      console.log(`Length: ${results.length}`)
+      const results = await getPaginatedData(filters, structure);               //sets varaible results to the value of getPaginatedData
+      console.log(`Length: ${results.length}`)                                  
       console.log('Data (first 7 items):', results.slice(0, 7));
-      var sliceinfo = results.slice(0, 1)
-      
-      let paragraphvariable = document.querySelector("p1");
+      var sliceinfo = results.slice(0, 1)                                       
+          
+      let paragraphvariable = document.querySelector("p1");                     
       
       var last7daysinfoinstringform = JSON.stringify(sliceinfo) 
       console.log(sliceinfo[0].Date);
@@ -84,13 +93,13 @@ var x = document.getElementById("searchbar").value;
 
 
 
-      document.getElementById("date").innerHTML = (sliceinfo[0].Date);
-      document.getElementById("name").innerHTML = (sliceinfo[0].Name);
-      document.getElementById("vaccinated").innerHTML = (sliceinfo[0].Vaccinated);
-      document.getElementById("vaccinatedtot").innerHTML = (sliceinfo[0].Vaccinatedtot);
-      document.getElementById("new").innerHTML = (sliceinfo[0].New);   
-      document.getElementById("cumulative").innerHTML = (sliceinfo[0].Cumulative);
-      document.getElementById("deaths").innerHTML = (sliceinfo[0].Deaths);
+      document.getElementById("date").innerHTML = (sliceinfo[0].Date);                                  //sets the innerHTML of date to the value of sliceinfo[0].Date
+      document.getElementById("name").innerHTML = (sliceinfo[0].Name);                                  //sets the innerHTML of name to the value of sliceinfo[0].Name
+      document.getElementById("vaccinated").innerHTML = (sliceinfo[0].Vaccinated);                      //sets the innerHTML of vaccinated to the value of sliceinfo[0].Vaccinated
+      document.getElementById("vaccinatedtot").innerHTML = (sliceinfo[0].Vaccinatedtot);                //sets the innerHTML of vaccinatedtot to the value of sliceinfo[0].Vaccinatedtot
+      document.getElementById("new").innerHTML = (sliceinfo[0].New);                                    //sets the innerHTML of new to the value of sliceinfo[0].New
+      document.getElementById("cumulative").innerHTML = (sliceinfo[0].Cumulative);                      //sets the innerHTML of cumulative to the value of sliceinfo[0].Cumulative
+      document.getElementById("deaths").innerHTML = (sliceinfo[0].Deaths);                              //sets the innerHTML of deaths to the value of sliceinfo[0].Deaths
 
      // Object.assign(document.querySelector('paragraphvariable.innerText').style, {
        // position: 'fixed',
@@ -102,12 +111,25 @@ var x = document.getElementById("searchbar").value;
     //})
   };  // main
 
-  main().catch(err => {
+  main().catch(err => {                                                                 //catches error
       console.error(err);
-      process.exitCode = 1;
+      process.exitCode = 1;                                                             //if there is an error, set process.exitCode to 1
 
       
   });
 
 }
 
+function contactForm() {                                                                //function for contact form
+                                                                                        //imports data from the html form as varaibles 
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
+
+                                                                                        //prints each variable to the console with some text saying what each value is
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+    console.log("Subject" + subject);
+    console.log("Message: " + message);
+}
--- run.js
+++ run.js
@@ -1,26 +1,23 @@
-const express = require('express');
+const express = require('express');                         // Express web server framework
 
-const app = express();
-const path = require('path');
-const public = path.join(__dirname,'public');
+const app = express();                                      // Create a new Express application
+const path = require('path');                               // Node.js built-in module for file paths
+const public = path.join(__dirname,'public');               // Path to public directory
 
-app.use(express.static(public));
+app.use(express.static(public));                            // Use public directory as static directory
 
-app.get('/', function(req, res)
-{ res.sendFile(path.join(public, 'about.html'));
+app.get('/', function(req, res)                             // Handle requests to the root URL
+{ res.sendFile(path.join(public, 'about.html'));            // Send the about.html file
 })
  
-app.get('/map', function(req, res)
-{ res.sendFile(path.join(public, 'map.html'));
+app.get('/map', function(req, res)                          // Handle requests to the map URL
+{ res.sendFile(path.join(public, 'map.html'));              // Send the map.html file
 })
 
-app.get('/about', function(req, res)
-{ res.sendFile(path.join(public, 'about.html'));
+app.get('/about', function(req, res)                        // Handle requests to the about URL
+{ res.sendFile(path.join(public, 'about.html'));            // Send the about.html file
 })
 
-app.get('/contact', function(req, res)
-{ res.sendFile(path.join(public, 'contact.html'));
-})
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000. Ctrl^c to quit.'); })
+app.listen(3000, () => {                                    // Listen on port 3000
+    console.log('Server started on port 3000. Ctrl^c to quit.'); })                     // Log to console
